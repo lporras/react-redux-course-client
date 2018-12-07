8fd81141727d389a19c1b2e60076a52b1e4f17a6
Luis Porras <lporras16@gmail.com>
2018-12-07 15:59:19 -0300

    display form errors

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,11 +2,22 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
-    renderInput (formProps) {
+    renderError({error, touched}) {
+        if(touched && error) {
+            return (
+                <div className="ui error message">
+                    <div className="header">{error}</div>
+                </div>
+            );
+        }
+    }
+    renderInput =  ({input, label, meta}) => {
+        const className = `field ${meta.error && meta.touched ? 'error' : ''}`
         return (
-            <div className="field">
-                <label>{formProps.label}</label>
-                <input {...formProps.input} />
+            <div className={className}>
+                <label>{label}</label>
+                <input {...input} autoComplete="off" />
+                {this.renderError(meta)}
             </div>
         );
     }
@@ -16,7 +27,7 @@ class StreamCreate extends React.Component {
     }
     render() {
         return (
-            <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+            <form className="ui form error" onSubmit={this.props.handleSubmit(this.onSubmit)}>
                 <Field name="title" component={this.renderInput} label="Title"/>
                 <Field name="description" component={this.renderInput} label="Description"/>
                 <button className="ui button primary">Submit</button>
@@ -25,6 +36,18 @@ class StreamCreate extends React.Component {
     }
 };
 
+const validate = (formValues) => {
+    const errors = {};
+    if (!formValues.title) {
+        errors.title = 'You must enter a title';
+    }
+    if(!formValues.description) {
+        errors.description = 'You must enter a description';
+    }
+    return errors;
+};
+
 export default reduxForm({
-    form: 'streamCreate'
+    form: 'streamCreate',
+    validate
 })(StreamCreate);
