cf161da0eff78177457f5142f0200ad6908a136a
Luis Porras <lporras16@gmail.com>
2018-12-10 12:57:03 -0300

    adding video tag and flv library

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import flv from 'flv.js';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
 
 class StreamShow extends React.Component {
+    constructor(props){
+        super(props);
+        this.videoRef = React.createRef();
+    }
     componentDidMount() {
         const id = this.props.match.params.id;
         this.props.fetchStream(id);
     }
     render() {
-        const { title, description } = this.props.stream;
         if (this.props.stream) {
+            const { title, description } = this.props.stream;
             return (
                 <div>
+                    <video ref={this.videoRef} style={{width: '100%'}} controls/>
                     <h1>{title}</h1>
                     <h5>{description}</h5>
                 </div>
