1ace30022ff182a4b6e4628f8b0cc167334e6a49
Luis Porras <lporras16@gmail.com>
2018-12-10 13:09:21 -0300

    adding video with flv

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -11,6 +11,22 @@ class StreamShow extends React.Component {
     componentDidMount() {
         const id = this.props.match.params.id;
         this.props.fetchStream(id);
+        this.buildPlayer();
+    }
+    componentDidUpdate() {
+        this.buildPlayer();
+    }
+    buildPlayer() {
+        if (this.player || !this.props.stream) {
+            return;
+        }
+        const id = this.props.match.params.id;
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        });
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.load();
     }
     render() {
         if (this.props.stream) {
